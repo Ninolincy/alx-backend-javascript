f19c462738cad7b32f51264f7730005327975873
Ninolincy <lincy3akoth@gmail.com>
2024-10-08 20:41:56 +0300

    data manipilation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+function getStudentsByLocation(students, city) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.filter(students => students.location === city);
+}
---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+function getStudentIdsSum(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }    
+    return students.reduce((sum, students) => sum + students.id)
+
+}
