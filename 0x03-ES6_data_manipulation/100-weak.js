4eeb3a57cf6f658f7e833b9a6fe92b9684a461b2
Ninolincy <lincy3akoth@gmail.com>
2024-10-08 20:11:52 +0300

    data manipilation

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,22 @@
+export const WeakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+    if (WeakMap.get(endpoint) >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+}
+
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+    if (!weakMap.has(endpoint)) {
+        weakMap.set(endpoint, 0);
+    }
+
+    const queryCount = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, queryCount);
+
+    if (queryCount >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+}
